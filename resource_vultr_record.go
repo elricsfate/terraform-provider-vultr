49ac21a0c475d84cb2b783924c1dde306010b0bf
Robert Tisdale <elricsfate@gmail.com>
2017-05-11 14:13:47 -0500

    Implementing DNS. Porting from CloudFlare provider. Initial commit.

---
+++ resource_vultr_record.go
@@ -0,0 +1,215 @@
+// Ported from Vultr provider
+// https://github.com/hashicorp/terraform/blob/master/builtin/providers/cloudflare/resource_cloudflare_record.go
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+        "github.com/JamesClonk/vultr/lib"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceVultrRecord() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceVultrRecordCreate,
+		Read:   resourceVultrRecordRead,
+		Update: resourceVultrRecordUpdate,
+		Delete: resourceVultrRecordDelete,
+
+		SchemaVersion: 1,
+		MigrateState:  resourceVultrRecordMigrateState,
+		Schema: map[string]*schema.Schema{
+			"domain": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"hostname": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"value": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"ttl": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+
+			"priority": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+
+//			"proxied": {
+//				Default:  false,
+//				Optional: true,
+//				Type:     schema.TypeBool,
+//			},
+
+			"zone_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceVultrRecordCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+
+	newRecord := cloudflare.DNSRecord{
+		Type:     d.Get("type").(string),
+		Name:     d.Get("name").(string),
+		Content:  d.Get("value").(string),
+//		Proxied:  d.Get("proxied").(bool),
+		ZoneName: d.Get("domain").(string),
+	}
+
+	if priority, ok := d.GetOk("priority"); ok {
+		newRecord.Priority = priority.(int)
+	}
+
+	if ttl, ok := d.GetOk("ttl"); ok {
+		newRecord.TTL = ttl.(int)
+	}
+
+	// Validate value based on type
+	if err := validateRecordName(newRecord.Type, newRecord.Content); err != nil {
+		return fmt.Errorf("Error validating record name %q: %s", newRecord.Name, err)
+	}
+
+	// Validate type
+	if err := validateRecordType(newRecord.Type, newRecord.Proxied); err != nil {
+		return fmt.Errorf("Error validating record type %q: %s", newRecord.Type, err)
+	}
+
+	zoneId, err := client.ZoneIDByName(newRecord.ZoneName)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", newRecord.ZoneName, err)
+	}
+
+	d.Set("zone_id", zoneId)
+	newRecord.ZoneID = zoneId
+
+	log.Printf("[DEBUG] Vultr Record create configuration: %#v", newRecord)
+
+	r, err := client.CreateDNSRecord(zoneId, newRecord)
+	if err != nil {
+		return fmt.Errorf("Failed to create record: %s", err)
+	}
+
+	// In the Event that the API returns an empty DNS Record, we verify that the
+	// ID returned is not the default ""
+	if r.Result.ID == "" {
+		return fmt.Errorf("Failed to find record in Create response; Record was empty")
+	}
+
+	d.SetId(r.Result.ID)
+
+	log.Printf("[INFO] Vultr Record ID: %s", d.Id())
+
+	return resourceVultrRecordRead(d, meta)
+}
+
+func resourceVultrRecordRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+	domain := d.Get("domain").(string)
+
+	zoneId, err := client.ZoneIDByName(domain)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", domain, err)
+	}
+
+	record, err := client.DNSRecord(zoneId, d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.SetId(record.ID)
+	d.Set("hostname", record.Name)
+	d.Set("type", record.Type)
+	d.Set("value", record.Content)
+	d.Set("ttl", record.TTL)
+	d.Set("priority", record.Priority)
+	d.Set("proxied", record.Proxied)
+	d.Set("zone_id", zoneId)
+
+	return nil
+}
+
+func resourceVultrRecordUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+
+	updateRecord := cloudflare.DNSRecord{
+		ID:       d.Id(),
+		Type:     d.Get("type").(string),
+		Name:     d.Get("name").(string),
+		Content:  d.Get("value").(string),
+		ZoneName: d.Get("domain").(string),
+		Proxied:  false,
+	}
+
+	if priority, ok := d.GetOk("priority"); ok {
+		updateRecord.Priority = priority.(int)
+	}
+
+	if proxied, ok := d.GetOk("proxied"); ok {
+		updateRecord.Proxied = proxied.(bool)
+	}
+
+	if ttl, ok := d.GetOk("ttl"); ok {
+		updateRecord.TTL = ttl.(int)
+	}
+
+	zoneId, err := client.ZoneIDByName(updateRecord.ZoneName)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", updateRecord.ZoneName, err)
+	}
+
+	updateRecord.ZoneID = zoneId
+
+	log.Printf("[DEBUG] Vultr Record update configuration: %#v", updateRecord)
+	err = client.UpdateDNSRecord(zoneId, d.Id(), updateRecord)
+	if err != nil {
+		return fmt.Errorf("Failed to update Vultr Record: %s", err)
+	}
+
+	return resourceVultrRecordRead(d, meta)
+}
+
+func resourceVultrRecordDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+	domain := d.Get("domain").(string)
+
+	zoneId, err := client.ZoneIDByName(domain)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", domain, err)
+	}
+
+	log.Printf("[INFO] Deleting Vultr Record: %s, %s", domain, d.Id())
+
+	err = client.DeleteDNSRecord(zoneId, d.Id())
+	if err != nil {
+		return fmt.Errorf("Error deleting Vultr Record: %s", err)
+	}
+
+	return nil
+}
